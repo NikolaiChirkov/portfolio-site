395f213172f672a84067e0217773f2e9b2693c3b
Nikolai Chirkov <qwertymonstro@gmail.com>
2021-02-24 16:37:12 +0300

    setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from './Header/Header';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <div>
-          <h1>Hello World</h1>
+          <Header />
         </div>
       </>
     )
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    
+  return (
+    <div className="header">
+      <img src="logo192.png" alt="portfolio logo"/>
+      <h1>Title</h1>
+    </div>
+  );
+    
+}
+
+export default Header;
