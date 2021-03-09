b5c4360d63d590e4556b45d47be3ec7ebb574d55
NikolaiChirkov <qwertymonstro@gmail.com>
2021-03-09 17:04:17 +0300

    wrap header face in navlink

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,13 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import Logo from '../../../resources/svg/portfolio.svg';
 import Navbar from './Navbar/Navbar';
 import './Header.scss';
 
 const Header = (): JSX.Element => (
   <header className="header">
-    <div className="header__face">
+    <NavLink to="/" className="header__face">
       <img className="header__logo" src={Logo} alt="portfolio logo" />
       <h1 className="header__title">Портфолио Николая</h1>
-    </div>
+    </NavLink>
     <Navbar />
   </header>
 );
