d26a2b65668b426dfb0ac568784b9ef63f14624a
NikolaiChirkov <qwertymonstro@gmail.com>
2021-03-12 13:34:06 +0300

    PS-6: add introduction Home page content

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,16 @@
-import Section from '../Common/Section/Section';
-import data from './data';
+// import Section from '../Common/Section/Section';
+// import data from './data';
 import './Home.scss';
 
 const Home = (): JSX.Element => (
   <main className="home">
-    {data.map((section) => (
-      <Section {...section} />
-    ))}
+    <section className="home_intr">
+      <h2 className="intr_title">Привет, я Николай</h2>
+      <h3 className="intr_subtitle">JavaScript разработчик</h3>
+    </section>
+    <section className="home_overview">
+      <h2 className="overview_header">Проекты, в которых учавствовал</h2>
+    </section>
   </main>
 );
 
