22c685c56e128ba6adcfdbc603d78710901639da
NikolaiChirkov <qwertymonstro@gmail.com>
2021-03-13 19:23:05 +0300

    PS-6: add contact block content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,43 @@
 import HHLogo from '../../resources/svg/hh-ru.svg';
-import GitHubLogo from '../../resources/svg/github.svg';
+import GitHubLogo from '../../resources/svg/i-github.svg';
+import MailLogo from '../../resources/svg/i-mail.svg';
+import PhoneLogo from '../../resources/svg/i-phone.svg';
+import Link from './Link/Link';
 import './Footer.scss';
 
 const Footer = (): JSX.Element => (
   <footer className="footer">
-    <section id="contact" className="contact_links">
-      <h2 className="links__header"></h2>
+    <section id="contacts" className="contact__links">
+      <h2 className="links__header">Способы связаться</h2>
       <div className="links__container">
-        <a
-          className="links_link"
-          href="https://spb.hh.ru/resume/5872d277ff062edf220039ed1f67657738596a"
-          target="_blank"
-        >
-          <img className="link_img" src={HHLogo} alt="hh.ru logo" />
-          <p className="link_caption">hh.ru</p>
-        </a>
-        <a
-          className="links_link"
-          href="https://github.com/NikolaiChirkov"
-          target="_blank"
-        >
-          <img className="link_img" src={GitHubLogo} alt="github logo" />
-          <p className="link_caption">GitHub</p>
-        </a>
+        <Link
+          linkHref="https://spb.hh.ru/resume/5872d277ff062edf220039ed1f67657738596a"
+          linkTarget="_blank"
+          linkSrc={HHLogo}
+          linkAlt="hh.ru logo"
+          linkCaption="Резюме"
+        />
+        <Link
+          linkHref="https://github.com/NikolaiChirkov"
+          linkTarget="_blank"
+          linkSrc={GitHubLogo}
+          linkAlt="github logo"
+          linkCaption="GitHub"
+        />
+        <Link
+          linkHref="tel:+79313331417"
+          linkTarget="_self"
+          linkSrc={PhoneLogo}
+          linkAlt="phone logo"
+          linkCaption="Телефон"
+        />
+        <Link
+          linkHref="mail:nikolyachirkov@yandex.ru"
+          linkTarget="_blank"
+          linkSrc={MailLogo}
+          linkAlt="mail logo"
+          linkCaption="Почта"
+        />
       </div>
     </section>
   </footer>
