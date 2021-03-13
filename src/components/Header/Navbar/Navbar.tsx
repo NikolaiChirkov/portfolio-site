18c427e8ea0e115fe47d8472a58de1cc4089d699
NikolaiChirkov <qwertymonstro@gmail.com>
2021-03-13 14:56:28 +0300

    PS-6: fix navbar-links

--- src/components/Header/Navbar/Navbar.tsx
+++ src/components/Header/Navbar/Navbar.tsx
@@ -3,10 +3,14 @@ import './Navbar.scss';
 const Navbar = (): JSX.Element => (
   <ul className="navbar">
     <li className="navbar__link">
-      <a href="#main">Главная</a>
+      <a className="navbar__link-a" href="#main">
+        Главная
+      </a>
     </li>
     <li className="navbar__link">
-      <a href="#overview">Проекты</a>
+      <a className="navbar__link-a" href="#overview">
+        Проекты
+      </a>
     </li>
   </ul>
 );
