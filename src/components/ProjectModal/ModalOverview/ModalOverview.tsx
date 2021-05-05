158c02c22d00f36fd0fbc1b4cdfe9d572db9e3fe
NikolaiChirkov <nchirkov@risensun.agency>
2021-05-05 07:44:34 +0300

    PS-6: minor changes & refactoring

--- src/components/ProjectModal/ModalOverview/ModalOverview.tsx
+++ src/components/ProjectModal/ModalOverview/ModalOverview.tsx
@@ -16,7 +16,7 @@ const ModalOverview = ({
     <div className="modal__overview-info">
       <h2 className="overview-info__text">Название проекта: {name}</h2>
       <h3 className="overview-info__text odd-line">Тип проекта: {type}</h3>
-      <p className="overview-info__text">{stack}</p>
+      <p className="overview-info__text">Технологии: {stack}</p>
       <p className="overview-info__text odd-line">
         Ссылка на сайт:
         <a className="overview-info__text-a" href={linkHref} target="_blank">
